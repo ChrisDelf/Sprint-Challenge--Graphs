914dbf1fc29542bf0ac337c2a7d7d4e4346708d5
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-01-23 16:05:02 -0500

    initial commit

--- adv.py
+++ adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-
+from util import Stack, Queue  # These may come in handy
 import random
 from ast import literal_eval
 
@@ -13,8 +13,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,6 +29,47 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+
+### helper
+def get_directions(room_id):
+
+    valid_directions=[]
+    if 'n' in room_id[1].keys():
+        valid_directions.append('n')
+
+    if 's' in room_id[1].keys():
+        valid_directions.append('s')
+
+    if 'e' in room_id[1].keys():
+        valid_directions.append('e')
+
+    if 'w' in room_id[1].keys():
+        valid_directions.append('w')
+
+    shuffle(valid_directions)
+
+    return valid_directions
+
+
+### DFT
+def dft_adv(starting_room):
+    #Create an empty stack and push starting vertex Id
+    stack = Stack()
+    stack.push([starting_room])
+    # Create a Set to store visited vertices
+    visited = {}
+    while stack.size() < len(room_graph):
+        path = stack.pop()
+        room_id = path[-1]
+        if room_id not in visited:
+
+            visited[room_id] = set(path)
+            print(visited)
+            for direction in get_directions(room_id):
+                visited[room_id].add(direction)
+                print(visited)
+
+dft_adv(player.current_room.id)
 
 
 # TRAVERSAL TEST
