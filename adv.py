87ff50bf57a51cadbf5df546a7062bbe0f33615b
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-01-24 13:34:08 -0500

    mvp have a bug where somethimes it gets stuck in a queue

--- adv.py
+++ adv.py
@@ -14,8 +14,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-#map_file = "maps/main_maze.txt"
+#map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -62,6 +62,34 @@ def get_directions(room_id):
     # if room_id == 9:
     #     print("HELLO", valid_directions)
     return valid_directions
+def check_next_room_exits(next_id, next_direction, prev_id):
+    # print("I'm running", next_id)
+
+    opposites = { 'w': 'e', 's': 'n', 'n': 's', 'e': 'w'}
+    next_array = []
+    pre_array = []
+    for d in room_graph[next_id][1].keys():
+        if d == 'n':
+            next_array.append('s')
+        if d == 's':
+            next_array.append('n')
+        if d == 'e':
+            next_array.append('w')
+        if d == 'w':
+            next_array.append('e')
+    for i in room_graph[next_id][1].keys():
+        pre_array.append(opposites[i])
+    if next_direction in next_array:
+        # print("we are good")
+        return True
+    else:
+        # print("warning!")
+        # print(next_direction)
+        return False
+
+
+
+
 
 
 ### DFT
@@ -95,9 +123,12 @@ def big_loop(starting_room):
     opposites = { 'w': 'e', 's': 'n', 'n': 's', 'e': 'w'}
     room_options = len(room_graph[starting_room][1].keys())
     moves = []
-    track_rooms = []
+    tracked_rooms = set()
     current_room = starting_room
-    while len(visited) < 17:
+
+    while len(visited) < 500:
+        print("STuck here? dft")
+
         while '?' in visited[current_room].values():
 
 
@@ -106,7 +137,8 @@ def big_loop(starting_room):
             player_room_directions = visited[current_room]
             array_possible_exits = []
             index = 0
-
+            # print("-------------------------->", visited[0])
+            # input(")))))))))))))))))))")
             for e in list(player_room_directions.values()):
                 if e == '?':
 
@@ -116,22 +148,73 @@ def big_loop(starting_room):
             if len(array_possible_exits) == 0:
                 print("dead end")
                 break
-
+            # print("-------------------------->", visited[0])
             next_direction = array_possible_exits[random.randrange(len(array_possible_exits))]
             # print("Next directions", next_direction)
             # print("dir array", array_possible_exits)
             next_room = room_graph[current_room][1][next_direction]
+            # print("prev room", current_room)
+            # print("next room!!!!",next_room, room_graph[next_room][1].keys())
+            next_room_check = check_next_room_exits(next_room, next_direction, current_room)
+
             # print("next_room", next_room)
+            # print("next_room_check", next_room_check)
+
             moves.append(next_direction)
-            visited[current_room][next_direction] = next_room
-            visited[next_room] = get_directions(next_room)
-            visited[next_room][opposites[next_direction]] = current_room
-            # print("Current Room", current_room)
+            if next_room_check == True:
+                # visited[next_room] = get_directions(next_room)
+                print(next_direction)
+                visited[current_room][next_direction] = next_room
+                if next_room not in visited:
+                    visited[next_room] = get_directions(next_room)
+                    print("here!!!!", visited[next_room])
+                    visited[next_room][opposites[next_direction]] = current_room
+                else:
+                    visited[next_room][opposites[next_direction]] = current_room
+            if next_room_check == False:
+                if next_room not in visited:
+                    # breakpoint()
+                    print("danger zone")
+                    visited[current_room][next_direction] = next_room
+                    visited[next_room] = get_directions(next_room)
+
+                # print(visited[next_room])
+                print("danger zone2")
+                # visited[current_room][next_direction] = next_room
+
+                # current_room = next_room
+            # print("room_check", next_room_check)
+            # print("current_room", current_room)
+            # print("next_room", next_room)
+            current_room = next_room
+
+
+                # visited[next_room] = get_directions(next_room)
+
+                # print(next_room)
+                # print(visited[next_room], "wrewrwrewrwerew")
+                # print(visited)
+
+
+                # print("false room", visited[next_room][next_direction].values())
+                # if 'f' in visited[next_room][next_direction].values():
+                #     break
+                #     print("false room", visited[next_room])
+                #
+
+
+            # world.print_rooms()
+            # print("visited", visited)
+            # input("-----------------------------")
+
+
             # world.print_rooms()
             # pretty(visited)
+
+            # current_room = next_room
+            # print("Current Room", current_room)
             # input("--------------------------------------------")
 
-            current_room = next_room
        # Create an empty queue and enqueue a PATH TO the starting vertex ID
             # Create a Set to store visited vertices
             # While the queue is not empty...
@@ -147,17 +230,20 @@ def big_loop(starting_room):
         # print("dead end time")
         # print("Current Room@#$@$@#$@#$@#$23", current_room)
         # print("roomid", visited)
-        while len(visited) < 17 and '?' not in visited[current_room].values():
-            breakpoint()
+        # print("dfs", moves)
+        while len(visited) < 500 and '?' not in visited[current_room].values():
+            print("STuck here?")
             queue = Queue()
             player_room_directions = visited[current_room]
             v = set()
             for key, value in player_room_directions.items():
                 queue.enqueue([value,[key]])
             while queue.size() > 0:
+                # print("stuck queue?")
                 this_one = queue.dequeue()
                 room = this_one[0]
                 if room not in v:
+                    # print(len(moves))
                     # print("visited_rooms", visited[room].values())
                     if '?' in visited[room].values():
                         # breakpoint()
@@ -165,18 +251,27 @@ def big_loop(starting_room):
                         moves += this_one[1]
                         break
                 v.add(room)
+
                 player_room_directions = visited[room]
                 for key, value in player_room_directions.items():
                     path_copy = this_one[1].copy()
                     path_copy.append(key)
                     queue.enqueue([value, path_copy])
-    print(len(moves))
+                    # print(queue.queue)
+
+    #         print("bfs", moves)
+    #         print("bfs", current_room)
+    #         print("--------------------------------")
+    #         # breakpoint()
+    # print("visited", v)
+        print(len(visited))
+
     return moves
 
-move = big_loop(player.current_room.id)
+moves_1 = big_loop(player.current_room.id)
 
 # print("move array", move)
-traversal_path = move
+traversal_path = moves_1
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -190,6 +285,7 @@ if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+    print(len(moves_1))
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
