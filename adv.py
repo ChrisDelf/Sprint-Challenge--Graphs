d9a129bd105a9f7ef70e8f24e257bf453976f89f
Chris-Delfaus <ChrisDelf@protonmail.com>
2020-01-23 19:49:30 -0500

    have dft kinda working

--- adv.py
+++ adv.py
@@ -4,6 +4,7 @@ from world import World
 from util import Stack, Queue  # These may come in handy
 import random
 from ast import literal_eval
+from random import shuffle
 
 # Load world
 world = World()
@@ -28,50 +29,95 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-
+print(room_graph[0])
 
 ### helper
 def get_directions(room_id):
 
-    valid_directions=[]
-    if 'n' in room_id[1].keys():
-        valid_directions.append('n')
+    valid_directions={}
+    if 'n' in room_graph[room_id][1].keys():
+        valid_directions['n'] = '?'
 
-    if 's' in room_id[1].keys():
-        valid_directions.append('s')
+    if 's' in room_graph[room_id][1].keys():
+        valid_directions['s'] = '?'
 
-    if 'e' in room_id[1].keys():
-        valid_directions.append('e')
+    if 'e' in room_graph[room_id][1].keys():
+        valid_directions['e'] = '?'
 
-    if 'w' in room_id[1].keys():
-        valid_directions.append('w')
+    if 'w' in room_graph[room_id][1].keys():
+        valid_directions['w'] = '?'
 
-    shuffle(valid_directions)
-
+    # shuffle(valid_directions)
+    print(valid_directions)
     return valid_directions
 
 
 ### DFT
+# def dft_adv(starting_room):
+#     #Create an empty stack and push starting vertex Id
+#     stack = Stack()
+#     stack.push([starting_room])
+#     # Create a Set to store visited vertices
+#     visited = {}
+#     # while stack.size() < len(room_graph):
+#     #     path = stack.pop()
+#     #     room_id = path[-1]
+#     #     if room_id not in visited:
+#     #
+#     #         visited[room_id] = {}
+#     #         print(visited)
+#     #         for get_directions(room_id):
+#     #             visited[room_id].add(direction)
+#     #             # visited[room_id][] = '?'
+#     #             print(visited[room_id])
+#     #         print(visited[room_id][1].keys())
+
 def dft_adv(starting_room):
     #Create an empty stack and push starting vertex Id
     stack = Stack()
-    stack.push([starting_room])
+
+    room_id = (starting_room)
+    print("roomid", room_id)
     # Create a Set to store visited vertices
     visited = {}
-    while stack.size() < len(room_graph):
-        path = stack.pop()
-        room_id = path[-1]
-        if room_id not in visited:
-
-            visited[room_id] = set(path)
-            print(visited)
-            for direction in get_directions(room_id):
-                visited[room_id].add(direction)
-                print(visited)
-
-dft_adv(player.current_room.id)
+    room_options = len(room_graph[0][1].keys())
+    move = []
+    track_rooms = []
+    while room_options > 1:
 
 
+
+        random_exit = random.randrange(0,4)
+        print(random_exit)
+        directions_list = [ 'n','s','e','w']
+        player_room_directions = get_directions(room_id)
+        movement = directions_list[random_exit]
+        player.travel(directions_list[random_exit])
+        move.append(movement)
+        #getting new room id
+        # path_copy = path.copy
+        # path_copy.append(room_id)
+        # track_rooms = path_copy
+        room_id = player.current_room.id
+        print("player", movement)
+        print("player_room_directions", player_room_directions)
+        print("player_room_id", room_id)
+        print("movement array", move)
+        room_options = len(player_room_directions)
+        # room_id = path[-1]
+        # if room_id not in visited:
+        #
+        #     visited[room_id] = {}
+        #     print(visited)
+        #     for direction in get_directions(room_id):
+        #         visited[room_id].add(direction)
+        #         # visited[room_id][] = '?'
+        #         print(visited[room_id])
+        #
+    return move
+move = dft_adv(player.current_room.id)
+
+traversal_path = move
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
